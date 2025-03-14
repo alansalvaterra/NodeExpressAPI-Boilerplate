e6fd74afd5a88d9b9d6b9044289d0d1390b151d4
alansalvaterra <alan.salvaterra@gmail.com>
2025-03-14 16:44:45 -0300

    Adicionando middlewares cors e helmet

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,25 @@
 import express from "express";
-import userRoutes from "./routes/userRoutes";
 import * as dotenv from "dotenv";
+import helmetMiddleware from "./middlewares/helmetMiddleware";
+import corsMiddleware from "./middlewares/corsMiddleware";
+import userRoutes from "./routes/userRoutes";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.json());
+const allowedOrigins = [
+  'https://meufrontend.com', // Frontend em produção
+  'http://localhost:3000',   // Desenvolvimento
+];
 
+// Middlewares
+app.use(express.json());
+app.use(helmetMiddleware);
+app.use(corsMiddleware);
+
+// Rotas
 app.use("/api", userRoutes);
 
 app.listen(PORT, () => {
